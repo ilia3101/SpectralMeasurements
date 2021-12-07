d3d8cecf619b7c1dd44efba8b7defb4a20502bfb
ilia3101 <23642861+ilia3101@users.noreply.github.com>
2021-12-07 21:10:34 +0000

    fix

--- DataProcessing/compile.sh
+++ DataProcessing/compile.sh
@@ -2,11 +2,11 @@ cd raw2dat_src
 gcc -c -O3 raw2dat.c
 gcc -c -O3 RawReader.c
 gcc raw2dat.o RawReader.o libraw_r.a -o raw2dat -lstdc++ -lm -lz
-mv raw2dat ../../raw2dat
+mv raw2dat ../raw2dat
 rm *.o
 cd ..
 
 cd process_data_src
 gcc process_data.c -o process_data
-mv process_data ../../process_data
+mv process_data ../process_data
 cd ..
